6fb0a8b7522ae740288aa878182c5dedb6807e2b
jrreed <jacob.reed@oracle.com>
2025-01-09 12:18:31 -0500

    up

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -4,14 +4,20 @@ return {
     completion = {
       list = {
         -- selection = "manual",
+        selection = {
+          preselect = true,
+          auto_insert = true,
+        },
       },
     },
     keymap = {
       ["<C-k>"] = { "select_prev", "fallback" },
       ["<C-j>"] = { "select_next", "fallback" },
       ["<Tab>"] = { "accept", "fallback" },
-      ["<Up>"] = { "scroll_documentation_up", "fallback" },
-      ["<Down>"] = { "scroll_documentation_down", "fallback" },
+      ["<C-l>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-h>"] = { "scroll_documentation_down", "fallback" },
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
       ["<S-Tab>"] = { "snippet_forward", "fallback" },
       ["<C-Tab>"] = { "snippet_backward", "fallback" },
       ["<C-e>"] = { "hide", "fallback" },
