3de49147d549953712ac1332ba621014e994475e
jrreed <jacob.reed@oracle.com>
2025-01-16 15:15:05 -0500

    up

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -35,7 +35,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.editor.illuminate" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
-    { import = "lazyvim.plugins.extras.editor.snacks_picker" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.go" },
