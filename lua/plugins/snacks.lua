e69dc98795d5bad505d975a465c23ac3efd1b040
jrreed <jacob.reed@oracle.com>
2025-02-28 16:06:24 -0500

    update dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,12 +3,13 @@ return {
   opts = {
     dashboard = {
       sections = {
-        {
-          section = "terminal",
-          cmd = "cat ~/.config/nvim/banner.txt",
-          height = 15,
-          padding = 1,
-        },
+        -- {
+        --   section = "terminal",
+        --   cmd = "cat ~/.config/nvim/banner.txt",
+        --   height = 15,
+        --   padding = 1,
+        -- },
+        { section = "header" },
         { section = "keys", gap = 1, padding = 1 },
         {
           pane = 2,
@@ -18,9 +19,9 @@ return {
           indent = 2,
           padding = 1,
           cwd = true,
-          limit = 8,
+          limit = 5,
         },
-        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        -- { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
         {
           pane = 2,
           icon = " ",
@@ -35,6 +36,47 @@ return {
           ttl = 5 * 60,
           indent = 3,
         },
+        function()
+          local in_git = Snacks.git.get_root() ~= nil
+          local cmds = {
+            {
+              title = "Open Issues",
+              cmd = "gh issue list -L 2",
+              key = "i",
+              action = function()
+                vim.fn.jobstart("gh issue list --web", { detach = true })
+              end,
+              icon = " ",
+              height = 7,
+            },
+            {
+              icon = " ",
+              title = "Open PRs",
+              cmd = "gh pr list -L 2",
+              key = "P",
+              action = function()
+                vim.fn.jobstart("gh pr list --web", { detach = true })
+              end,
+              height = 7,
+            },
+            {
+              icon = " ",
+              title = "Git Status",
+              cmd = "git --no-pager diff --stat -B -M -C",
+              height = 10,
+            },
+          }
+          return vim.tbl_map(function(cmd)
+            return vim.tbl_extend("force", {
+              pane = 2,
+              section = "terminal",
+              enabled = in_git,
+              padding = 1,
+              ttl = 60,
+              indent = 3,
+            }, cmd)
+          end, cmds)
+        end,
         { section = "startup", pane = 1 },
       },
       preset = {
